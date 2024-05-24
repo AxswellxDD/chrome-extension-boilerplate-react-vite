c7d3e5ae7bfebfcacc8498761a3e4049ad58fcf7
Dawid Paluch <mowmidave2006@gmail.com>
2024-05-24 12:36:29 +0200

    Add some messeges, that's explain the code

--- src/pages/devtools/index.ts
+++ src/pages/devtools/index.ts
@@ -4,11 +4,8 @@ try {
   console.error(e);
 }
 
+// This event listener is triggered when the extension is first installed or when it is updated.
+// In this case, it logs a simple message to the console.
 chrome.runtime.onInstalled.addListener(() => {
   console.log('hello');
-});
-
-chrome.runtime.onMessage.addListener(function (request, sender, response) {
-  console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
-  if (request.greeting === 'hello') response({ farewell: 'goodbye' });
 });
--- src/pages/sidepanel/index.tsx
+++ src/pages/sidepanel/index.tsx
@@ -12,6 +12,9 @@ function init() {
 
 init();
 
+// This code sends a message to the background script with a greeting.
+// Upon receiving the message, the background script sends a response with a farewell message.
+
 chrome.runtime.sendMessage({ greeting: 'Hello' }, function (response) {
   console.log(response.farewell);
 });
